3dc54ba314c7ce30f1324f23944293cc6e5f1024
IsraelTello <itello@rapi.tech>
2026-04-19 03:18:51 -0500

    req invitado

--- src/models/Guest.java
+++ src/models/Guest.java
@@ -1,5 +1,36 @@
 package models;
 
-public class Guest {
-    
+public class Guest 
+{
+    private String name;
+    private String cellphone;
+    private String gender;
+    private String birthDate;
+    private String address;
+    private boolean acceptedTerms;
+
+    @Override
+    public String toString() 
+    {
+        return name + ";" + cellphone + ";" + gender + ";" + birthDate + ";" 
+             + address + ";" + (acceptedTerms ? "1" : "0");
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCellphone() { return cellphone; }
+    public void setCellphone(String cellphone) { this.cellphone = cellphone; }
+
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
+
+    public String getBirthDate() { return birthDate; }
+    public void setBirthDate(String birthDate) { this.birthDate = birthDate; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public boolean isAcceptedTerms() { return acceptedTerms; }
+    public void setAcceptedTerms(boolean acceptedTerms) { this.acceptedTerms = acceptedTerms; }
 }
--- src/models/GuestIO.java
+++ src/models/GuestIO.java
@@ -1,5 +1,61 @@
 package models;
 
-public class GuestIO {
-    
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import core.Model;
+import core.View;
+
+public class GuestIO implements Model
+{
+    private static final String DIRECTORY = ".";
+    private static final String FILE = "guests.txt";
+
+    private List<View> views = new ArrayList<>();
+    private String notice;
+
+    @Override
+    public void attach(View view) 
+    {
+        views.add(view);
+    }
+
+    @Override
+    public void detach(View view) 
+    {
+        views.remove(view);
+    }
+
+    @Override
+    public void notifyViews() 
+    {
+        for (View v : views) {
+            v.update(this, notice);
+        }
+    }
+
+    public void saveGuest(Guest guest) throws Exception
+    {
+        try {
+            BufferedWriter writer = new BufferedWriter(
+                new FileWriter(new File(DIRECTORY, FILE), true)
+            );
+            writer.write(guest.toString());
+            writer.newLine();
+            writer.close();
+
+            notice = "Invitado registrado correctamente";
+            notifyViews();
+        } catch (FileNotFoundException fnfe) {
+            notice = "Archivo no encontrado";
+            notifyViews();
+        } catch (Exception ex) {
+            notice = "Error al guardar invitado";
+            notifyViews();
+        }
+    }
 }
